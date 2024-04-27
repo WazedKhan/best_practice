7a2565a6516cd86f00bdcda701725c9ca531c3a2
WazedKhan <wazedkhan119399@gmail.com>
2024-04-27 22:21:57 +0600

    finding unique item in list with default dict

---
+++ code/deafault_dict/find_unique_item.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import List
+
+
+def singleNumber(nums: List[int]):
+    # create a empty dict with default dict
+    counts = defaultdict(int)
+    # count appears of every items in the list
+    for num in nums:
+        counts[num] += 1
+
+    # check if any item appeared once
+    for key, value in counts.items():
+        if value == 1:
+            return key
+
+
+# list
+arr = [1, 2, 2]
+single_number = singleNumber(arr)
+print(single_number)
