c23a0ecc8a1694af1e3568172872af6c77fcf8c5
WazedKhan <wazedkhan119399@gmail.com>
2024-06-30 22:21:37 +0600

    feat: character class

---
+++ code/OOP/Chronicles_of_Valor/character.py
@@ -0,0 +1,14 @@
+class Character:
+    def __init__(self, name: str, health: int, attack_power: int) -> None:
+        self._name = name
+        self._health = health
+        self._attack_power = attack_power
+
+    def attack(self, other):
+        self._health -= self._attack_power
+
+    def is_alive(self):
+        return self._health > 0
+
+    def __str__(self):
+        return f"{self._name} - Health: {self._health}"
